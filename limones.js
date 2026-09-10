d5315bb5f62b147765b18548c83d6e57fb5ece66
jaevit17 <jaevit17@gmail.com>
2026-09-10 18:30:09 -0500

    mover a la Izquierda

--- limones.js
+++ limones.js
@@ -4,6 +4,7 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
 
 function iniciar(){
     dibujarSuelo();
@@ -15,5 +16,18 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="red";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+//mover Izquierda 
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
 }
